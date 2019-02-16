8152762bc11cb259f487b2bf00ce5d005d8f5e6e
NestorCruzQ <44757376+NestorCruzQ@users.noreply.github.com>
2019-02-15 21:15:30 -0500

    nivelFormacion

--- app/nivelFormacion.php
+++ app/nivelFormacion.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\softDeletes;
 class nivelFormacion extends Model
 {
-	use softDeletes;
+		use softDeletes;
     protected $dates = ['deleted_at'];
     protected $table='acad_nivel_formacion';
     protected $fillable = [
-        'id', 'nombre','id_usu_cre', 'fecha_cre', 'id_usu_mod', 'fecha_mod'
+        'id', 'nombre', 'id_usu_cre', 'fecha_cre', 'id_usu_mod', 'fecha_mod',
     ];
 }
